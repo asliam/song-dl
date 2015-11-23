852084781015c6b4d99b0386737bc1f15f42debb
Matthew Conlen <mc@mathisonian.com>
2015-11-22 17:21:23 -0800

    initial commit

---
+++ index.js
@@ -0,0 +1,57 @@
+var Promise = require("bluebird");
+var cli = require('cli');
+var BestVideo = require('youtube-best-video');
+var ytdl = require('ytdl-core');
+var ffmpeg = require('fluent-ffmpeg');
+
+
+var downloadSong = function(query, outputFile, key, cb) {
+
+  getYoutubeUrl(query, key, function(err, link) {
+    if(err) {
+      cb && cb(err);
+      return;
+    }
+
+    var stream = ytdl(link);
+    ffmpeg(stream)
+      .save(outputFile)
+      .on('end', function() {
+        cb && cb(null);
+      });
+  });
+}
+
+
+var getYoutubeUrl = function(query, key, cb) {
+  var bestVideo = BestVideo(key)
+  bestVideo.findBestMusicVideo(query, function(err, video) {
+    if(err) {
+      cb(err);
+    } else {
+      cb(null, video.link);
+    }
+  });
+};
+
+if (require.main === module) {
+  cli.parse({
+      output: [ 'o', 'Output filename', 'file', "song.mp3"],
+      key: ['k', 'Youtube API Key', 'string']
+  });
+
+  cli.main(function(args, options) {
+
+    var query = args.join(' ');
+    if(query.trim() === '') {
+      console.warn('Please provide a search term.');
+      process.exit();
+    }
+
+    downloadSong(query, options.output, options.key);
+  });
+} else {
+  // expose functions if this file has been
+  // required from elsewhere
+  module.exports = downloadSong;
+}
